39d164d4ae8d14c0fdb96fddc31c6db0584392fc
zhukovka <helen.zhukova@gmail.com>
2018-05-31 18:00:34 +0300

    shaders

--- start/index.ts
+++ start/index.ts
@@ -1,3 +1,22 @@
 import WebGL from "../webgl/index.js";
 
 const gl = new WebGL(document.getElementById("canvas"));
+
+// Vertex shader program
+
+const vsSource = `
+    attribute vec4 aVertexPosition;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
--- webgl/index.ts
+++ webgl/index.ts
@@ -21,4 +21,77 @@ export default class WebGL {
         gl.clear(gl.COLOR_BUFFER_BIT);
         this.gl = gl;
     }
+
+    /**
+     * creates a shader of the given type, uploads the source and compiles it.
+     * @param type
+     * @param source {string}
+     * @returns {WebGLShader}
+     */
+    loadShader (type: number, source: string): WebGLShader {
+        const shader = this.gl.createShader(type);
+
+        // Send the source to the shader object
+
+        this.gl.shaderSource(shader, source);
+
+        // Compile the shader program
+
+        this.gl.compileShader(shader);
+
+        // See if it compiled successfully
+
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            alert('An error occurred compiling the shaders: ' + this.gl.getShaderInfoLog(shader));
+            this.gl.deleteShader(shader);
+            return null;
+        }
+
+        return shader;
+    }
+
+    /**
+     * creates a shader of the vertex type, uploads the source and compiles it.
+     * @param {string} source
+     * @returns {WebGLShader}
+     */
+    loadVertexShader (source: string): WebGLShader {
+        return this.loadShader(this.gl.VERTEX_SHADER, source);
+    }
+
+    /**
+     * creates a shader of the fragment type, uploads the source and compiles it.
+     * @param {string} source
+     * @returns {WebGLShader}
+     */
+    loadFragmentShader (source: string): WebGLShader {
+        return this.loadShader(this.gl.FRAGMENT_SHADER, source);
+    }
+
+    /**
+     * Initialize a shader program, so WebGL knows how to draw our data
+     * @param shaders
+     * @returns {WebGLProgram}
+     */
+    initShaderProgram (...shaders: WebGLShader[]): WebGLProgram {
+
+        // Create the shader program
+
+        const shaderProgram = this.gl.createProgram();
+
+        for (const shader of shaders) {
+            this.gl.attachShader(shaderProgram, shader);
+        }
+        this.gl.linkProgram(shaderProgram);
+
+        // If creating the shader program failed, alert
+
+        if (!this.gl.getProgramParameter(shaderProgram, this.gl.LINK_STATUS)) {
+            alert('Unable to initialize the shader program: ' + this.gl.getProgramInfoLog(shaderProgram));
+            return null;
+        }
+
+        return shaderProgram;
+    }
+
 }
